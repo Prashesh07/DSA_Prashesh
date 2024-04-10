64eb3e7d6b749fabf146ac2d3df4d913d1d4fa93
Prashesh <aprashesh@gmail.com>
2024-04-10 09:58:52 +0545

    first

--- Linked_list/header/linked_list.h
+++ Linked_list/header/linked_list.h
@@ -11,3 +11,22 @@ class Node
   Node(int d,Node *next ): info(d),next(next) {}
 };
 
+class LinkedList 
+{
+    private:
+    Node* HEAD;
+    Node* TAIL;
+
+    public:
+    LinkedList();
+    ~LinkedList();
+
+    void add(Node *pred,int data);
+    void addToHead(int data);
+    void addToTail(int data);
+
+    bool remove(int data);
+    bool removeFromHead(int &data);
+    bool removeFromTail(int &data);
+
+}
